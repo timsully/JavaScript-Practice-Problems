d6cfa54d95affb8aa621e841cd88bbfefc91b3b5
Tim Sullivan <timsullivan97@gmail.com>
2018-09-12 21:37:02 -0700

    created instances of Assassin class and a method on prototype of the class

--- arrays-objects.js
+++ arrays-objects.js
@@ -8,7 +8,7 @@ const studentInfo = {
     knownsJS: false,
     sayHi: function() {
         console.log('Hello! I am ' + this.name);
-    }
+    },
 };
 
 // calling the function which will output the code
@@ -27,3 +27,79 @@ function testFn() {
 }
 
 testFn(true, 'Shovel', 17);
+
+
+// const ninjaAssassin = {
+//     codename: "Hollow Eyes",
+//     weapon: "katana",
+//     stealthy: true,
+//     headCount: 0,
+//     kill: function() {
+//         this.headCount++;
+//     },
+// };
+
+// // overwrite the headCount method with int value of 45
+// ninjaAssassin.headCount = 45;
+// // accessing the kill method in the object ninjaAssassin
+// ninjaAssassin.kill();
+// // 
+// ninjaAssassin.codename.toUpperCase();
+
+
+// const characterAssassin = {
+//     codename: 'The Politican',
+//     weapon: 'rhetoric',
+//     stealthy: false,
+//     headCount: 0,
+//     kill: function() {
+//         this.headCount++;
+//     },
+// };
+
+
+// const ghostAssassin = {
+//     codename: 'The Wraith',
+//     weapon: 'spirit magic',
+//     stealthy: true,
+//     headCount: 0,
+//     kill: function () {
+//         this.headCount++;
+//     },
+// };
+
+
+const Assassin = function(codename, weapon, stealthy) {
+    this.codename = codename;
+    this.weapon = weapon;
+    this.stealthy = stealthy;
+    this.headCount = 0;
+    // What not to do
+    // this.kill = function() {
+    //     this.headCount++;
+    // };
+};
+
+Assassin.prototype.kill = function() {
+    this.headCount++;
+};
+
+// Instances of the Assassin class
+const ninjaAssassin = new Assassin('Hollow Eyes', 'katana', true);
+const characterAssassin = new Assassin('The Politician','rhetoric', false);
+const ghostAssassin = new Assassin('The Wraith', 'spirit magic', true);
+
+console.log(ninjaAssassin);
+console.log(characterAssassin);
+console.log(ghostAssassin);
+
+
+const array = [ 2, 6, 3, 4 ];
+const chars = [ 'a', 'd', 'j' ];
+Array.prototype.returnSecond = function() {
+    const secondItem = this[1];
+    return secondItem;
+};
+
+array.returnSecond();
+chars.returnSecond();
